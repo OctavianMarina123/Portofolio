f27a9aafa475edd469696e929651fce188edb803
Octa Marina <octamarina@Octas-Mac-mini.local>
2024-09-16 23:08:31 +0300

    Created the navbar and started the hero section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">Hi, I'm Octavian
+                    Marina</p>
+                <p className="hero_tag text-gray_gradient">Developer Merging AI, Web, and 3D Technologies</p>
+            </div>
+            <di></di>
+        </section>
+    )
+}
+export default Hero
