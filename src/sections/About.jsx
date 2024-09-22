c6faf0033a7659c789c330e98b7ae883f3f63464
Octa Marina <octamarina@Octas-Mac-mini.local>
2024-09-22 03:35:02 +0300

    Changed about and workexperience section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -13,6 +13,7 @@ import Button from "../components/Button.jsx";
 const About = () => {
     const [hasCopied, setHasCopied] = useState(false);
     const handleCopy = () =>{
+        console.log("copy")
         navigator.clipboard.writeText('octamarina@gmail.com')
         setHasCopied(true)
         setTimeout(()=>{
@@ -40,13 +41,35 @@ const About = () => {
 
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container relative">
-                        <FaLayerGroup
-                            className="absolute inset-0 w-full h-full text-gray-500"
-                            style={{opacity: 0.1, fontSize: '10rem'}}
-                        />
+                        <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+                            <Globe height={326} width={326} backgroundColor="rgba(0,0,0,0)"
+                                   backgroundImageOpacity={0.5}
+                                   showAtmosphere
+                                   showGraticules
+                                   globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                                   bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                            />
+
+                        </div>
+                        <p className="grid-headtext">Available for remote work worldwide.</p>
+                        <p className="grid-subtext">
+                            I'm based in Cluj-Napoca, Romania, with remote work available.
+                        </p>
+                        <a href={"#contact"}>
+                            <Button name="Contact Me" isBeam containerClass="w-full mt-10"/>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="col-span-1 xl:row-span-4">
+                    <div className="grid-container">
                         <div className="relative">
+                            <FaLayerGroup
+                                className="absolute inset-0 w-full h-full text-gray-500"
+                                style={{opacity: 0.1, fontSize: '10rem'}}
+                            />
                             <div>
-                                <p className="grid-headtext">Tech Stack</p>
+                                <p className="grid-headtext my-12">Tech Stack</p>
                                 <ul className="grid-subtext list-disc list-inside">
                                     <li>
                                         <strong>Native iOS Development:</strong> UIKit, SwiftUI
@@ -106,26 +129,6 @@ const About = () => {
                     </div>
                 </div>
 
-                <div className="col-span-1 xl:row-span-4">
-                    <div className="grid-container">
-                        <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-                            <Globe height={326} width={326} backgroundColor="rgba(0,0,0,0)"
-                                   backgroundImageOpacity={0.5}
-                                   showAtmosphere
-                                   showGraticules
-                                   globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                                   bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                            />
-
-                        </div>
-                            <p className="grid-headtext">Available for remote work worldwide.</p>
-                            <p className="grid-subtext">
-                                I'm based in Cluj-Napoca, Romania, with remote work available.
-                            </p>
-                        <Button name="Contact Me" isBeam containerClass="w-full mt-10"/>
-                    </div>
-                </div>
-
                 <div className="xl:col-span-2 xl:row-span-3">
                     <div className="grid-container">
                         <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
@@ -139,7 +142,7 @@ const About = () => {
 
                 <div className="xl:col-span-1 xl:row-span-2">
                     <div className="grid-container">
-                        <img
+                    <img
                             src="assets/grid4.png"
                             alt="grid-4"
                             className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
